73ec38d4bb50212c2e711089442de10ba13ad45c
= <jrohlandt@gmail.com>
2016-08-18 19:01:32 +0200

    temporary dependency on jquery, need to replace with something smaller

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div id="app"></div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="/js/jquery.js"></script>
         <script src="/js/bundle.js"></script>
     </body>
 </html>
