43990ab747ec04c0e76e027e34d3317735cdfcd1
= <jrohlandt@gmail.com>
2016-09-08 10:03:56 +0200

    clean up

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,11 +5,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
         <link rel="stylesheet" href="/css/style.css" charset="utf-8">
-        <style media="screen">
-            #map {
-                height: 300px;
-            }
-        </style>
     </head>
     <body>
         <div id="app"></div>
@@ -25,9 +20,6 @@
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCq5NUFKaqLtZ1ex07s16NAcCRxYDil5LE&callback=initMap"
             async defer
         >
-        </script>
-        <script type="text/javascript">
-
         </script>
         <script src="/js/bundle.js"></script>
     </body>
