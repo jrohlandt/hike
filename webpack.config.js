f11c0ef1cd1f13a46cf05208253ce39c0b8bf827
= <jrohlandt@gmail.com>
2016-09-15 11:25:43 +0200

    env: development

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   plugins: [
     new WebpackNotifierPlugin(),
     new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     new ExtractTextPlugin('../css/style.css', {
         allChunks: true
